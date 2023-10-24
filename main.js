654a64027d870e63b7cb4a9a9394a48267b000e5
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2023-10-24 12:59:01 +0300

    added a legend at the start

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const gameStart = document.querySelector('.game-start');
+const gameStart = document.querySelector('.start-menu');
 const gameScore = document.querySelector('.game-score');
 const gameArea = document.querySelector('.game-area');
 const gameOver = document.querySelector('.game-over');
